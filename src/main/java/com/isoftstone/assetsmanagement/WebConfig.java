d3e3469daa30a09f048f5c1db265cd404fe6879d
aya-elbellaoui <ayaella010@gmail.com>
2026-01-14 17:10:23 +0800

    Add WebConfig class to enable CORS

---
+++ src/main/java/com/isoftstone/assetsmanagement/WebConfig.java
@@ -0,0 +1,20 @@
+package com.isoftstone.assetsmanagement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // allow all paths
+                        .allowedOrigins("http://localhost:8080") // Vue frontend
+                        .allowedMethods("*"); // allow GET, POST, PUT, DELETE, etc.
+            }
+        };
+    }
+}
